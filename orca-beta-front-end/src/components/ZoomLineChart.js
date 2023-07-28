7b7ee23a9f82293c27c4c92f465c46decbfd9256
justin.pank <justin.pank@falconexodynamics.com>
2023-07-28 14:50:07 -0700

    zoomable now!

--- orca-beta-front-end/src/components/ZoomLineChart.js
+++ orca-beta-front-end/src/components/ZoomLineChart.js
@@ -22,6 +22,47 @@ class ZoomLineChart extends React.Component {
             }
           ],
         options: {
+            chart: {
+                type: 'area',
+                stacked: false,
+                height: 350,
+                zoom: {
+                  type: 'x',
+                  enabled: true,
+                  autoScaleYaxis: true
+                },
+                toolbar: {
+                  autoSelected: 'zoom'
+                }
+              },
+              dataLabels: {
+                enabled: false
+              },
+              markers: {
+                size: 0,
+              },
+              title: {
+                text: 'Stock Price Movement',
+                align: 'left'
+              },
+              fill: {
+                type: 'gradient',
+                gradient: {
+                  shadeIntensity: 1,
+                  inverseColors: false,
+                  opacityFrom: 0.5,
+                  opacityTo: 0,
+                  stops: [0, 90, 100]
+                },
+              },
+              tooltip: {
+                shared: false,
+                y: {
+                  formatter: function (val) {
+                    return (val / 1000000).toFixed(0)
+                  }
+                }
+              },
             xaxis: {
               categories: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
             },
