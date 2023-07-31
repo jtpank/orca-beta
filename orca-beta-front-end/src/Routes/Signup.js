68387a411f1b17d1e1fdd311af3c8d462406b312
justin.pank <justin.pank@falconexodynamics.com>
2023-07-31 15:04:37 -0700

    signup page with form complete

--- orca-beta-front-end/src/Routes/Signup.js
+++ orca-beta-front-end/src/Routes/Signup.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import NavBar from '../components/NavBar';
 import Footer from '../components/Footer';
+import SignupForm from '../components/SignupForm';
 class Signup extends React.Component {
     render() {
         return (
             <main class="flex-shrink-0">
                 <NavBar></NavBar>
                 <h1>Signup page</h1>
+                <SignupForm></SignupForm>
                 <Footer></Footer>
             </main>
         );
