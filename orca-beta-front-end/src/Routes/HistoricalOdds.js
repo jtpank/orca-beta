915f133b544950863e3ea227393b6c50729cf45d
justin.pank <justin.pank@falconexodynamics.com>
2023-07-31 15:17:11 -0700

    basic data table example render

--- orca-beta-front-end/src/Routes/HistoricalOdds.js
+++ orca-beta-front-end/src/Routes/HistoricalOdds.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import NavBar from '../components/NavBar';
 import Footer from '../components/Footer';
+import DataTable from '../components/DataTable';
 class HistoricalOdds extends React.Component {
     render() {
         return (
             <main class="flex-shrink-0">
                 <NavBar></NavBar>
                 <h1>Historical Odds DB</h1>
+                <DataTable></DataTable>
                 <Footer></Footer>
             </main>
         );
