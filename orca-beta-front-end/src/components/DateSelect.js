8fb2620339b3759f2f43d49a23341c9bba6a12d6
Justin Pank <justinpank@Justins-MacBook-Pro-2.local>
2023-09-15 15:51:18 -0700

    updated to include holidays array

--- orca-beta-front-end/src/components/DateSelect.js
+++ orca-beta-front-end/src/components/DateSelect.js
@@ -18,7 +18,13 @@ class DateSelect extends React.Component {
                 minDate={new Date()}
                 format="yyyy-MM-dd'T'HH:mm:ss'Z'"
                 placeholderText="Please select date"
-                holidays={[]}
+                holidays={[
+                  { date: "2023-08-15", holidayName: "India's Independence Day" },
+                  { date: "2023-12-31", holidayName: "New Year's Eve" },
+                  { date: "2023-12-25", holidayName: "Christmas" },
+                  { date: "2024-01-01", holidayName: "New Year's Day" },
+                  { date: "2023-11-23", holidayName: "Thanksgiving Day" }
+                ]}
                 />
                 {this.props.selectedDate && <p>Selected Date: {this.props.selectedDate.toISOString()}</p>}
             </div>
