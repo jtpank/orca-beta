30962475376f98efb98710123b12b0e1a712ec74
Justin Pank <justinpank@Justins-MacBook-Pro-2.local>
2023-09-15 16:22:50 -0700

    config

---
+++ orca-beta-front-end/src/config.js
@@ -0,0 +1,13 @@
+module.exports = global.config = {
+    // i18n: {
+    //     welcome: {
+    //         en: "Welcome",
+    //         fa: "خوش آمدید"
+    //     }
+    //     // rest of your translation object
+    // }
+    api_server: {
+        "localhost:5000"
+    }
+    // other global config variables you wish
+};
